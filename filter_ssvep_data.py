38a0c782ad3faa213e37c66d796e56f20548d661
Claire Leahy <cleahy@uvm.edu>
2024-03-08 09:34:31 -0500

    Commit files

---
+++ filter_ssvep_data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  8 09:07:48 2024
+
+@author: Ron Bryant and Claire Leahy
+"""
+
---
+++ test_filter_ssvep_data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  8 09:08:22 2024
+
+@author: Ron Bryant and Claire Leahy
+"""
+
